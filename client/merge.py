fc9be68350394d7bb6676e51540c0e598a2e31bd
Michal Hrbek <michal@dee.cz>
2026-03-01 16:34:30 +0100

    File selection

--- client/merge.py
+++ client/merge.py
@@ -1,15 +1,19 @@
 from glob import glob
 import os
+import sys
 from main import gen_filename
 
-dirname = os.path.join(os.path.dirname(__file__), "out/*.csv")
-fname = gen_filename()
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        dirname = os.path.join(os.path.dirname(__file__), "out/*.csv")
+        readlist = list(glob(dirname))
+    else:
+        readlist = sys.argv[1:]
 
-readlist = list(glob(dirname))
-
-with open(fname, "w") as wf:
-    with open(readlist[0], "r") as rf:
-        wf.write(rf.read())
-    for i in readlist[1:]:
-        with open(i, "r") as rf:
-            wf.write("\n".join(rf.readlines()[1:]))
+    fname = gen_filename()
+    with open(fname, "w") as wf:
+        with open(readlist[0], "r") as rf:
+            wf.write(rf.read())
+        for i in readlist[1:]:
+            with open(i, "r") as rf:
+                wf.write("\n".join(rf.readlines()[1:]))
